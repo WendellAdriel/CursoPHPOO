86b92f30d2ee81b48c694f70d455f70e3b407a6f
Wendell Adriel <wendell@codeshare.com.br>
2015-06-19 22:12:46 -0300

    Aula 1 Curso PHP OO

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+	class Casa
+	{
+		public $tamanho;
+		public $valor;
+
+		public function setTamanho($tamanho)
+		{
+			$this->tamanho = $tamanho;
+		}
+
+		public function getValor()
+		{
+			return $this->valor;
+		}
+	}
+
+	$objeto = new Casa();
+	$objeto->tamanho = "50 metros quadrados";
+	$objeto->valor = "R$100.000,00";
+
+	$objeto2 = new Casa();
+	$objeto2->tamanho = "100 metros quadrados";
+	$objeto2->valor = "R$180.000,00";
+
+	$objeto->setTamanho("200 metros quadrados");
+	echo $objeto2->getValor();
+	var_dump($objeto);
+	var_dump($objeto2);
+?>
